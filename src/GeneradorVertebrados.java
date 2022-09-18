c5880be020580e5770820141516ca80ad152bcc6
natalyquintana <natalyquintana@uninorte.edu.co>
2022-09-17 20:17:43 -0500

    Agregue 'Cocodrilo, Reptil

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,8 +3,8 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Perro", "Mamifero"); 
-        miVertebrados[2] = new Vertebrado("Gato", "Mamifero");
+        miVertebrados[2] = new Vertebrado("Cocodrilo", "Reptil");
+    
         return miVertebrados;
     }
 }
